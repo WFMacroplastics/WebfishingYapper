265f49a161b6a081a006623974b54264afe7e5e1
Awesomerly <Awesomerly@users.noreply.github.com>
2024-11-15 16:24:49 -0500

    scene_filter: ignore texturerect and send text instead

--- project/mods/Yapper/scene_filter.gd
+++ project/mods/Yapper/scene_filter.gd
@@ -16,10 +16,12 @@ func _scene_filterer(node:Node):
 	# This took hours of trial and error to narrow down as many edge cases as possible...
 	# I wish there was a better way to do this, but for many things, there just isn't
 	# If you find anything that makes this easier, PLEASE ping us on the Webfishing modding discord.
-	if node is RichTextLabel:
+	if node is TextureRect:
+		node.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	elif node is RichTextLabel:
 		node.mouse_filter = Control.MOUSE_FILTER_PASS
 #		print("RichTextLabel - ", node.get_class(), " ", node.bbcode_text)
-		_connect(node, "bbcode_text")
+		_connect(node, "text")
 	elif node is Label and not node.name == "stack_count":
 		node.mouse_filter = Control.MOUSE_FILTER_PASS
 #		print("Label - ", node.text)
@@ -30,7 +32,7 @@ func _scene_filterer(node:Node):
 	elif node.get("bbcode_text"):
 		node.mouse_filter = Control.MOUSE_FILTER_PASS
 #		print(node.get_class(), " - ", node.text)
-		_connect(node, "bbcode_text")
+		_connect(node, "text")
 	elif node.get("text") and not node.name == "stack_count":
 #		print(node.get_class(), " - ", node.text)
 		_connect(node, "text")
